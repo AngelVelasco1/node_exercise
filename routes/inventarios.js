c89f22e7ad9452480ac60f7739ca5340f41168d3
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-07-08 11:52:07 -0500

    feat: :sparkles: Insert records into inventarios

--- routes/inventarios.js
+++ routes/inventarios.js
@@ -22,5 +22,37 @@ storageInventarios.use((req, res, next) => {
     }   
 });
 
+//? Insert records
+storageInventarios.post('/', (req, res) => {
+    const { id_producto, id_bodega, cantidad } = req.body;
 
+    try {
+        const actionSelect = 'SELECT * FROM inventarios WHERE id_producto = ? AND id_bodega = ?';
+        conx.query(actionSelect, [id_producto, id_bodega], (result) => {
+            if (result > 0) {
+                let currentCantidad = result[0].cantidad;
+                const newCantidad = currentCantidad + cantidad;
+                
+                const actionUpdate = 'UPDATE inventarios SET cantidad = ?';
+                conx.query(actionUpdate, [newCantidad, id_producto, id_bodega], (result) => {
+                    res.json(JSON.stringify(result));
+                })
+            }
+            else {
+                const actionInsert = 'INSERT INTO inventarios (id_producto, id_bodega, cantidad) VALUES (4,5, 6)';
+                conx.query(actionInsert, (result) => {
+                    res.json(JSON.stringify(result));
+                })
+            }
+        });
+        
+
+    } catch (err) {
+        console.error('Error en la consulta: ', err);
+        res.status(500);
+    }
+
+})
+/* (id_producto, id_bodega, cantidad) VALUES (1, 1, 1)
+ */
 export default storageInventarios;
